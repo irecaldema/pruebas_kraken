d843fb8b73dc600e32fb93c2bbb22220ba14b4a9
IosuR <irecaldema14dw@ikzubirimanteo.com>
2015-12-09 13:25:41 +0000

    0.01

--- six-technologies/controllers/index.js
+++ six-technologies/controllers/index.js
@@ -4,8 +4,9 @@ var IndexModel = require('../models/index');
 //var userModel = require('../models/userModel');
 var Usuario = require('../models/Usuario');
 //var Usuario = require('../models/Usuario2');
+var bcrypt = require('bcrypt');
 
-var md5 = require('md5');
+//var md5 = require('md5');
 
 module.exports = function (router) {
 
@@ -17,21 +18,29 @@ module.exports = function (router) {
     });*/
     
     router.get('/', function (req, res) {  
-        res.render('login', model);     
+        res.render('login', model);  
     });    
-
-    router.post('/', function (req, res) {
-        //var Usuario = new Usuario();
+    
+    router.get('/home', function (req, res) {  
+        res.render('home', model);     
+    });
+    
+    // login usuario
+    router.post('/login', function (req, res) {
         var form_usuario=req.body.usuario;
         var form_pass=req.body.pass;
         console.log(form_pass);
-        form_pass=md5(form_pass);
+        
+        //encriptacion en hash
+        var salt = bcrypt.genSaltSync(10);
+        var pass_coded = bcrypt.hashSync(form_pass, salt);
+        //form_pass=md5(form_pass);
         
         console.log(form_usuario);
-        console.log(form_pass);
+        console.log(pass_coded);
         
         //buscar usuario en base de datos y si esta validado cookie de sesion mas redirijir a home
-        Usuario.findOne({ usuario: form_usuario, pass: form_pass }, function (err, usuario) {
+        Usuario.findOne({ usuario: form_usuario, pass: pass_coded }, function (err, usuario) {
         //Usuario.find(function (err, users) {    
             if (err) {
                 console.error(err);
@@ -47,7 +56,9 @@ module.exports = function (router) {
                 //res.json(Usuario);
                 if(usuario.validated){
                     //guardar sesion en cookie y redirigir
-                    res.render('home', model); 
+                    //res.render('home', model);
+                    //redireccionar
+                    res.redirect('/home');
                 }else{
                     res.render('login', model); 
                 } 
@@ -57,4 +68,42 @@ module.exports = function (router) {
         });    
         
     });
+    // registrar usuario
+    router.post('/register', function (req, res) {
+        var form_usuario = req.body.usuario;
+        var form_email = req.body.email;
+        var form_pass = req.body.pass;
+        var form_pass2 = req.body.pass2;
+        
+        var salt = bcrypt.genSaltSync(10);
+        var pass_coded = bcrypt.hashSync(form_pass, salt);
+        console.log(bcrypt.compareSync(form_pass2, pass_coded));
+        console.log(pass_coded);
+        
+        console.log(form_usuario);
+        console.log(form_email);
+        console.log(form_pass);
+        console.log(form_pass2);
+        
+        if (bcrypt.compareSync(form_pass2, pass_coded)){
+            var chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            var new_key = "";
+            for (var i = 0; i < 32; i++) {
+                new_key += chars[Math.floor(Math.random()*35)];
+            }
+            console.log("key: " + new_key);
+            
+            var usuario = new Usuario({ usuario : form_usuario, pass : pass_coded, email : form_email, activacion_key : new_key, validated : 0});
+            
+            usuario.save(function (err) {
+                if (err) {
+                    console.log('save error', err);
+                }
+            });
+            
+        } else {
+            console.log('Las pass no es la misma');
+        }
+
+    });    
 };
--- six-technologies/models/Usuario.js
+++ six-technologies/models/Usuario.js
@@ -31,26 +31,7 @@ var userModel = function () {
         }
     };
     
-/*pruebas
-     userSchema.methods.find({ 'name.last': 'Ghost' }, 'name occupation', function (err, person) {
-  if (err) return handleError(err);
-  console.log('%s %s is a %s.', person.name.first, person.name.last, person.occupation) // Space Ghost is a talk show host.    
-    return mongoose.model('userModel', userSchema);
-
-})
-    }*/ 
     return mongoose.model('Usuario', userSchema);
 };
 
 module.exports = new userModel();
-
-//module.exports = mongoose.model('Usuario', userSchema);
-
-//en proceso
-//http://stackoverflow.com/questions/14307953/mongoose-typeerror-on-a-models-findone-method
-//http://mongoosejs.com/docs/guide.html
-//https://github.com/lmarkus/Kraken_Example_Shopping_Cart/blob/8ff3100bb06f9051270767432e3b51be4b9ee83f/README.md
-//http://mongoosejs.com/docs/api.html#model_Model.find
-
-//http://stackoverflow.com/questions/24172405/typeerror-object-has-no-method-find-when-using-mongoose-with-express
-//module.exports = mongoose.model('userModel', userSchema)
